e4a0fce9cf4dbc1e82b186c673bd5308ddd32cb5
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-05-25 12:36:13 +0200

    Feat: Add handleSubmit function to form

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,13 +1,40 @@
+import { useState } from "react";
+
 export const LoginView = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const handleSubmit = (event) => {
+    // this prevents the default behaviour of the form which is to reload the entire page
+    event.preventDefault();
+
+    const data = {
+      username: username, 
+      password: password
+    };
+
+    fetch('https://myflix-kjb92.herokuapp.com/login', {
+      method: "POST",
+      body: JSON.stringify(data)
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
         Username: 
-        <input type="text" />
+        <input 
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        />
       </label>
       <label>
         Password:
-        <input type="password" />
+        <input type="password" 
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        />
       </label>
       <button type="submit">Submit</button>
     </form>
