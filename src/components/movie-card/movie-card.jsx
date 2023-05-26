fc70571aba6cad7977d3fb22f5995c42497254c7
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-05-26 18:14:46 +0200

    feat: Add React-Bootstrap card component

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,14 +1,30 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
+    <Card 
+      style={{ width: '18rem' }}
       onClick={() => {
-        onMovieClick(movie);
-      }}
+            onMovieClick(movie);
+          }}
     >
-      {movie.title}
-    </div>
+      <Card.Img variant="top" src={movie.image} />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>
+          {movie.description}
+        </Card.Text>
+        <Button variant="primary">Open</Button>
+      </Card.Body>
+    </Card>
+    // <div
+    //   onClick={() => {
+    //     onMovieClick(movie);
+    //   }}
+    // >
+    //   {movie.title}
+    // </div>
   );
 };
 
