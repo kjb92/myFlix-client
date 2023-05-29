e4c31eb123ffe4e405e7799626ba1bd695babc6a
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-05-29 15:49:28 +0200

    feature: Create NavigationBar

---
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -0,0 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Link from 'react-router-dom';
+
+export const NavigationBar = ({ user, onLoggedOut }) => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">myFlix</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+            <Nav.Link as={Link} to="/">Signup</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
