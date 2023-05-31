e21bc6ce507b931bffd6170343984f2842a893a8
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-05-31 10:00:18 +0200

    style: Update styling

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -1,39 +1,52 @@
 import PropTypes from 'prop-types';
 import { MovieCard } from '../movie-card/movie-card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
-export const FavoriteMovies = ({ favoriteMovies, username, token }) => {
+export const FavoriteMovies = ({ user, token, updateUser, movies }) => {
+  const favoriteMovieList = movies.filter((m) => user.favoriteMovies.includes(m._id));
 
   return (
-    <div>
-      <h3>Favorite Movies</h3>
-      {favoriteMovies.length > 0 ? (
-        favoriteMovies.map((movie) => (
-          <MovieCard key={movie._id} movie={movie} username={username} token={token} />
-        ))
-      ) : (
-        <p>No favorite movies found.</p>
-      )}
-    </div>
+    <>
+      <Row>
+        <Col>
+          <h3>My Favorite Movies</h3>
+        </Col>
+      </Row>
+      <Row>
+        {user.favoriteMovies.length > 0 ? (
+          favoriteMovieList.map((movie) => (
+            <Col className="mb-5" key={movie._id} xs={12} sm={8} md={6} lg={4} xl={3} xxl={3}>
+              <MovieCard movie={movie} user={user} token={token} updateUser={updateUser} favoriteMovies={user.favoriteMovies}/>
+            </Col>
+          ))
+        ) : (
+        <Col>
+          <p>No favorite movies found.</p>
+        </Col>
+        )}
+      </Row>
+    </>
   );
 };
 
 // Here is where we define all the props constraints
-FavoriteMovies.propTypes = {
-  favoriteMovies: PropTypes.arrayOf(
-    PropTypes.shape({
-      _id: PropTypes.string.isRequired,
-      title: PropTypes.string.isRequired,
-      description: PropTypes.string.isRequired,
-      image: PropTypes.string.isRequired,
-      director: PropTypes.shape({
-        name: PropTypes.string.isRequired,
-      }).isRequired,
-      genre: PropTypes.arrayOf(
-        PropTypes.shape({
-          name: PropTypes.string.isRequired,
-        })
-      ).isRequired,
-      featured: PropTypes.bool.isRequired,
-    })
-  ).isRequired
-};
+// FavoriteMovies.propTypes = {
+//   favoriteMovies: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       _id: PropTypes.string.isRequired,
+//       title: PropTypes.string.isRequired,
+//       description: PropTypes.string.isRequired,
+//       image: PropTypes.string.isRequired,
+//       director: PropTypes.shape({
+//         name: PropTypes.string.isRequired,
+//       }).isRequired,
+//       genre: PropTypes.arrayOf(
+//         PropTypes.shape({
+//           name: PropTypes.string.isRequired,
+//         })
+//       ).isRequired,
+//       featured: PropTypes.bool.isRequired,
+//     })
+//   ).isRequired
+// };
