20de00f4aeb676aad97a24f21da3cbb780d0fcc4
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-05-31 09:59:38 +0200

    style: Adjust styling

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -4,10 +4,11 @@ import { Form, Button } from 'react-bootstrap';
 import { FavoriteMovies } from './favorite-movies';
 import { UserInfo } from './user-info';
 import { MovieCard } from '../movie-card/movie-card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
-export const ProfileView = ({ user, token, movies }) => {
+export const ProfileView = ({ user, token, movies, updateUser, handleLogout, favoriteMovieList, favoriteMovies }) => {
   const [userProfile, setUserProfile] = useState(null);
-  const [favoriteMovies, setFavoriteMovies] = useState([]);
 
   //Fetch current user's profile information
   useEffect(() => {
@@ -15,60 +16,71 @@ export const ProfileView = ({ user, token, movies }) => {
       return;
     }
 
-    fetch('https://myflix-kjb92.herokuapp.com/users', {
+    fetch(`https://myflix-kjb92.herokuapp.com/users/${user.username}`, {
       method: 'GET',
       headers: { 
+        "Content-Type" : "application/JSON",
         Authorization: `Bearer ${token}`
       }
     })
       .then((response) => response.json())
       .then((data) => {
-        setUserProfile(data.find((u) => u.username === user.username));
-        setFavoriteMovies(movies.filter((m) => userProfile.favoriteMovies.includes(m._id)));
+        setUserProfile(data);
       })
       .catch((error) => {
         console.error('Error fetching user profile:', error);
       });
-  }, [userProfile]);  
+  }, [token]);  
 
   return (
-    <div>
-      <h2>Profile</h2>
-      <UserInfo user={user} token={token}/>
-      <FavoriteMovies favoriteMovies={favoriteMovies} username={user.username} token={token}/>
-    </div>
+    <>
+      <Row>
+        <Col>
+          <h2>Profile</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={5}>
+          <UserInfo user={user} userProfile={userProfile} token={token} updateUser={updateUser} handleLogout={handleLogout} />
+        </Col>
+      </Row>
+      <Row>
+        <Col className="mt-5"></Col>
+      </Row>
+      <Row>
+        <Col>
+          <FavoriteMovies user={user} token={token} updateUser={updateUser} movies={movies} />
+        </Col>
+      </Row>
+    </>
   );
 };
 
 // Here is where we define all the props constraints for the ProfileView
-ProfileView.propTypes = {
-  user: PropTypes.shape({
-      _id: PropTypes.string.isRequired,
-      username: PropTypes.string.isRequired,
-      email: PropTypes.string.isRequired,
-      password: PropTypes.string.isRequired,
-      favoriteMovies: PropTypes.arrayOf(
-        PropTypes.shape({
-          _id: PropTypes.string.isRequired
-        })
-      ).isRequired,
-      }).isRequired,
-  token: PropTypes.string.isRequired,
-  movies: PropTypes.arrayOf(
-    PropTypes.shape({
-      _id: PropTypes.string.isRequired,
-      title: PropTypes.string.isRequired,
-      description: PropTypes.string.isRequired,
-      image: PropTypes.string.isRequired,
-      director: PropTypes.shape({
-        name: PropTypes.string.isRequired,
-      }).isRequired,
-      genre: PropTypes.arrayOf(
-        PropTypes.shape({
-          name: PropTypes.string.isRequired,
-        })
-      ).isRequired,
-      featured: PropTypes.bool.isRequired,
-    })
-  ).isRequired
-};
+// ProfileView.propTypes = {
+//   user: PropTypes.shape({
+//       username: PropTypes.string.isRequired,
+//       email: PropTypes.string.isRequired,
+//       password: PropTypes.string.isRequired,
+//       favoriteMovies: PropTypes.arrayOf(PropTypes.string.isRequired)
+//       .isRequired,
+//       }).isRequired,
+//   token: PropTypes.string.isRequired,
+//   movies: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       _id: PropTypes.string.isRequired,
+//       title: PropTypes.string.isRequired,
+//       description: PropTypes.string.isRequired,
+//       image: PropTypes.string.isRequired,
+//       director: PropTypes.shape({
+//         name: PropTypes.string.isRequired,
+//       }).isRequired,
+//       genre: PropTypes.arrayOf(
+//         PropTypes.shape({
+//           name: PropTypes.string.isRequired,
+//         })
+//       ).isRequired,
+//       featured: PropTypes.bool.isRequired,
+//     })
+//   ).isRequired
+// };
