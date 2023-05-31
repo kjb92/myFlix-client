671bb764f33be566524804b57f6c7bdd8579a43a
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-05-31 10:00:33 +0200

    refactor: Change variable naming

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -4,7 +4,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
 
-export const NavigationBar = ({ user, onLoggedOut }) => {
+export const NavigationBar = ({ user, handleLogout }) => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
@@ -16,7 +16,7 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
               <Nav className="me-auto">
                 <Nav.Link as={Link} to="/">Home</Nav.Link>
                 <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
-                <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
+                <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
               </Nav>
             ) : (
               <Nav className="me-auto">
